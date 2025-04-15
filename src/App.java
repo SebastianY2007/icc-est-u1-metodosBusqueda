4d5729f83dc540694755136c67f1dedc7a466214
Sebastian <derlis567y@gmail.com>
2025-04-15 07:40:57 -0500

    Primer commit

---
+++ src/App.java
@@ -0,0 +1,7 @@
+import controllers.MetodosBusqueda;
+
+public class App {
+    private MetodosBusqueda metodosBusqueda;
+    public static void main(String[] args) throws Exception {
+    }
+}
---
+++ src/controllers/MetodosBusqueda.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import views.ShowConsole;
+
+public class MetodosBusqueda {
+    private ShowConsole showConsole;
+    private int arreglo;
+    public int busqueaLineal(int valor){
+
+    return valor;
+    }
+}
